127e102076bb00005106d6133aadbbe9f1a5f3e2
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-07-29 10:10:31 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -13,14 +13,33 @@ function getRandomNumber(max) {
   
   // Anda, chacho, píntame en la consola un número random hasta 100;
   const myRandomNumber = getRandomNumber(100);
-  
   console.log(`Mi número aleatorio es ${myRandomNumber}`);
-
-  
+  let numCounter = 0;
 
 function NumberComp () {
-    console.log(input.value);
+
+    const newNumber = parseInt(input.value);
+    console.log(newNumber);
+
+    let content = `<p>¡HAS GANADO, CAMPEONA!</p>`
+    numCounter += 1;
+    counter.innerHTML = numCounter;
+
+
+
+    if (myRandomNumber === newNumber){
+        feedback.innerHTML = content;
+    } else if (myRandomNumber < newNumber){
+        content = `<p>El número es demasiado alto</p>`
+        feedback.innerHTML = content;
+    } else {
+        content = `<p>El número es demasiado bajo</p>`
+        feedback.innerHTML = content;
+    }
+
 }
+
+
 
 
 button.addEventListener('click', NumberComp);
