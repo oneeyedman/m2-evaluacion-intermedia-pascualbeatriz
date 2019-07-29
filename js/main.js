61b453a33cb4ab22c839e99e2608ea9a37108e60
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-07-29 08:42:53 +0200

    add folder structure, html structure and const in js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const input = document.querySelector('.play__instructions-gap_number');
+const button = document.querySelector('.play__button');
+const feedback = document.querySelector('.play__feedback');
+const counter = document.querySelector('.play__counter');
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('hola');
